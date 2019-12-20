21531da56c32eb0542e9bbfdd7dbe03ded337fc4
suprajapaidipala <59077819+suprajapaidipala@users.noreply.github.com>
2019-12-20 15:08:45 +0530

    Adding Project Files

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //console.log("hello world!")
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(express.static('frontend'))
 
